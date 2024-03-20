d1eaf0be45a41bd8a8f519d0076933fe3784c9c3
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-03-19 23:19:41 -0500

    se agrega js

--- db-eternal/index/app.js
+++ db-eternal/index/app.js
@@ -1,27 +1,59 @@
-let user_name = prompt("Ingresa tu nombre de guerrero.");
-let user_race = prompt("Ingresa tu raza, guerrero. Recuerda, esta página está personalizada si eres Sayayin, Humano, Androide, Mutante, Shin-Jin, Angel o Hakai-Shin");
-let user_maxt = prompt("Ingrese su transformación mas fuerte");
+
+// function hola_name(){
+//     return user_name;
+// }
+let user_name = prompt ("Bienvenido guerrero, dime tu nombre.");
+
+
+function raza (){
+
+    let user_race = prompt("Hola " + user_name + " Ingresa tu raza, guerrero. Recuerda, esta página está personalizada si eres Sayayin, Humano, Androide, Mutante, Shin-Jin, Angel o Hakai-Shin");
+
+    if (user_race === "Sayayin") {
+        document.body.style.backgroundColor = "black";
+        console.log(user_race);
+        document.write("Genial, un sayayin");
+    } else if (user_race === "Humano") {
+        document.body.style.backgroundColor = "green";
+        document.write("Genial, un Humano");
+    } else if (user_race === "Androide") {
+        document.body.style.backgroundColor = "blue";
+        document.write("Genial, un Androide");
+    } else if (user_race === "Mutante") {
+        document.body.style.backgroundColor = "purple";
+        document.write("Genial, un Mutante");
+    } else if (user_race === "Shin-Jin") {
+        document.body.style.backgroundColor = "orange";
+        document.write("Genial, un kaio");
+    } else if (user_race === "Angel") {
+        document.body.style.backgroundColor = "white";
+        document.write("Genial, un Angel");
+    } else if (user_race === "Hakai-Shin") {
+        document.body.style.backgroundColor = "red";
+        document.write("Genial, un Dios");
+    } 
+   
+}
+
+function poder(){
+    let user_power = prompt("Ingrese su nivel de poder máximo");
+
+    if (user_power<1000){
+        
+        alert('Insecto!! No aceptamos debiluchos! Entrena y vuelve cuando tengas un nivel de poder más alto >.>');
+        poder();
+   } else if (user_power>1000){
+    document.write("Disfruta tu estancia guerrero de clase baja");
+   } else if(user_power>10000){
+    document.write("Disfruta tu estancia guerrero de clase alta");
+   } 
+}
 
 
 
 
-document.write('Hola ' + user_name + ' representante de la raza ' + user_race + ', ostenta tu transformación ' + user_maxt + ' con orgullo.');
 
-if (user_race === "Sayayin") {
-    document.body.style.backgroundColor = "black";
-} else if (user_race === "Humano") {
-    document.body.style.backgroundColor = "green";
-} else if (user_race === "Androide") {
-    document.body.style.backgroundColor = "blue";
-} else if (user_race === "Mutante") {
-    document.body.style.backgroundColor = "purple";
-} else if (user_race === "Shin-Jin") {
-    document.body.style.backgroundColor = "orange";
-} else if (user_race === "Angel") {
-    document.body.style.backgroundColor = "white";
-} else if (user_race === "Hakai-Shin") {
-    document.body.style.backgroundColor = "red";
-} 
+
 
 
 
